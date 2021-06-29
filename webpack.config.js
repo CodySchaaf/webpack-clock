1b1c9092ae3b62e4d4ca37b89e5630d0125fea2b
CodySchaaf <codyjschaaf@gmail.com>
2021-06-28 18:55:42 -0700

    add style loader

--- webpack.config.js
+++ webpack.config.js
@@ -4,6 +4,14 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 module.exports = {
   mode: "development",
   entry: "./src/index.js",
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
   plugins: [
     new HtmlWebpackPlugin({
       template: "./src/index.html",
