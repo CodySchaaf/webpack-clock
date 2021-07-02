46a5907b318be02318803dedb95fec473d9a6c8d
CodySchaaf <codyjschaaf@gmail.com>
2021-07-01 17:49:13 -0700

    finish styles

--- src/Clock/index.js
+++ src/Clock/index.js
@@ -1,3 +1,5 @@
+import './style.css';
+
 export function Clock({ time }) {
   document.querySelector(".clock").innerHTML = time;
 }
--- src/Title/index.js
+++ src/Title/index.js
@@ -1,3 +1,5 @@
+import './style.css';
+
 export function Title({ title }) {
   document.querySelector(".title").innerHTML = title;
 }
