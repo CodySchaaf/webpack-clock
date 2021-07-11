856b1f1431d5f642245abefa99e482d7ebfc9e6a
CodySchaaf <codyjschaaf@gmail.com>
2021-07-10 19:35:31 -0700

    add html loader

--- webpack.config.js
+++ webpack.config.js
@@ -4,6 +4,14 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 module.exports = {
   mode: "development",
   entry: "./src/index.js",
+  module: {
+    rules: [
+      {
+        test: /\.(html)$/,
+        use: ["html-loader"],
+      },
+    ],
+  },
   plugins: [
     new HtmlWebpackPlugin({
       template: "./src/index.html",
