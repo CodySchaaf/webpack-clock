5ef83f9152e4a156168faf9a230b777548643457
CodySchaaf <codyjschaaf@gmail.com>
2021-07-10 20:53:36 -0700

    adds babel

--- webpack.config.js
+++ webpack.config.js
@@ -4,6 +4,17 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 module.exports = {
   mode: "development",
   entry: "./src/index.js",
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+        },
+      },
+    ],
+  },
   plugins: [
     new HtmlWebpackPlugin({
       template: "./src/index.html",
