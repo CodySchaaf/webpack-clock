43bce3cf7cafbc201f57e9125a55625c17793f20
CodySchaaf <codyjschaaf@gmail.com>
2021-07-10 20:16:45 -0700

    adds images via css

--- webpack.config.js
+++ webpack.config.js
@@ -4,6 +4,18 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 module.exports = {
   mode: "development",
   entry: "./src/index.js",
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: "asset/resource",
+      },
+    ],
+  },
   plugins: [
     new HtmlWebpackPlugin({
       template: "./src/index.html",
